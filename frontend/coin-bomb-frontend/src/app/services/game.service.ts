2337d4e910b989242ba99f32351fb4dcda258966
prasanth4181 <saiprasanth.bodaballa@slu.edu>
2026-01-07 02:46:58 -0600

    Fix Netlify deployment settings and Angular imports

--- frontend/coin-bomb-frontend/src/app/services/game.service.ts
+++ frontend/coin-bomb-frontend/src/app/services/game.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { GameInitDTO, SquareDTO, SquareRevealDTO } from '../models/game-and-square-dtos.interface';
+import { GameStateDTO } from '../models/game-and-square-dtos.interface';
 
 @Injectable({
   providedIn: 'root'
